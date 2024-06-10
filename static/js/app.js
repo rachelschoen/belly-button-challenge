6a3e6804c439b5d3cccf169b4580578167069aad
Rachel Schoen <161248655+rachelschoen@users.noreply.github.com>
2024-06-10 19:23:36 -0400

    update app.js with bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -66,6 +66,38 @@ function demo(selectedValue) {
     });
   }
 
+// Make the bar chart
+function bar(selectedValue) {
+    // Fetch the JSON data and console log it
+    d3.json(url).then((data) => {
+        console.log(`Data: ${data}`);
+
+        // An array of sample objects
+        let samples = data.samples;
+
+        // Filter data where id = selected value 
+        let filteredData = samples.filter((sample) => sample.id === selectedValue);
+
+        // Assign the first object to obj variable
+        let obj = filteredData[0];
+        
+        // Trace for the data for the horizontal bar chart
+        let trace = [{
+            // Slice the top 10 otus
+            x: obj.sample_values.slice(0,10).reverse(),
+            y: obj.otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
+            text: obj.otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            marker: {
+                color: "rgb(166,172,237)"
+            },
+            orientation: "h"
+        }];
+        
+        // Use Plotly to plot the data in a bar chart
+        Plotly.newPlot("bar", trace);
+    });
+}
 
 // Toggle to new plots when option changed
 function optionChanged(selectedValue) {
